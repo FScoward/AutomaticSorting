c24d29be1bcac89d58ce64d3098415d0191f6e11
FScoward <endlick1989@yahoo.co.jp>
2012-05-29 22:14:14 +0900

    first

---
+++ src/fscoward/model/Sorting.scala
@@ -0,0 +1,30 @@
+package fscoward.model
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.util.Date
+
+class Sorting {
+  /*
+   * ファイル移動 
+   * move: Path, Path -> Unit
+   */
+  def move(from: Path, to: Path) = {
+    Files.move(from, to, StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING)
+    val date = "%tY/%<tm/%<td %<tR" format new Date
+    writeToLog(date + " : write to : " + to)
+  }
+  
+  /*
+   * ログへ書き込み
+   * writeToLog: String -> Boolean
+   */
+  def writeToLog(log: String): Boolean = {
+    // log.txt に書き出す
+    val filename = "log.txt"
+    
+    true
+  }
+
+}
---
+++ src/fscoward/view/AutomaticSorting.scala
@@ -0,0 +1,9 @@
+package fscoward.view
+
+object AutomaticSorting {
+
+  def main(args: Array[String]): Unit = {
+    // guiでファイルを受け取りファイル名を関数に渡し、どこに移動させるかを決める
+  }
+
+}
