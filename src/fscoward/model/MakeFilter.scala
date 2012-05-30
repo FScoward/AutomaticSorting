54adaf0731dc3b839ce9d68aab79d03ffc284900
FScoward <endlick1989@yahoo.co.jp>
2012-05-31 00:01:02 +0900

    20120531

---
+++ src/fscoward/model/MakeFilter.scala
@@ -0,0 +1,24 @@
+package fscoward.model
+import java.nio.file.Path
+
+class MakeFilter(dir: Path) {
+  /*
+   * ファイル名取得
+   * takeFileName -> Array[String]
+   * */
+  def takeFileName = {
+    
+  }
+  
+  /*
+   * 共通部分の取り出し(変動部分 第何回 とかの排除）
+   * extraction -> 
+   * */
+  def extraction = {
+    // コンペアとって共通部分を見る?
+    
+    
+  }
+  
+  
+}
--- src/fscoward/view/AutomaticSorting.scala
+++ src/fscoward/view/AutomaticSorting.scala
@@ -3,7 +3,6 @@ package fscoward.view
 object AutomaticSorting {
 
   def main(args: Array[String]): Unit = {
-    // guiでファイルを受け取りファイル名を関数に渡し、どこに移動させるかを決める
   }
 
 }
